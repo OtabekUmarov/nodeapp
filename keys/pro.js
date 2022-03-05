ebf059ffc34b59e6f2a3871581753ebe59b370bc
OtabekUmarov <otabekumarov0220@gmail.com>
2022-03-05 11:25:26 +0500

    some

--- keys/pro.js
+++ keys/pro.js
@@ -5,10 +5,10 @@
 //     PASSWORD_EMAIL: process.env.PASSWORD_EMAIL
 // }
 
-
 module.exports = {
     BASE_URL: 'localhost',
-    MONGODB_URI: 'mongodb://127.0.0.1:27017/school',
+    MONGODB_URI: 'mongodb+srv://Zyn1e:a2z0i0z2@cluster0.ovlub.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+
     SESSION_SECRET: 'some secret key',
     SYSTEM_EMAIL: 'umarovotabek0220@gmail.com',
     PASSWORD_EMAIL: 'Otabek1999#'
